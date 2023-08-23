b9f560721bfa731e84e886f1914911238478e6e6
agenious1122 <ageenious@gmail.com>
2023-08-23 11:42:50 +0500

    variables difiend

--- Q6.js
+++ Q6.js
@@ -1,4 +1,11 @@
 "use strict";
 // Q 6:  Write a program that calculates the result of a 
 // mathematical expression.	((10 + 5) * 3 - 2) / (4 % 3)  - 7)
-console.log(((10 + 5) * 3 - 2) / (4 % 3) - 7);
+var num1 = 10;
+var num2 = 5;
+var num3 = 3;
+var num4 = 2;
+var num5 = 4;
+var num6 = 7;
+var result = (((10 + 5) * 3 - 2) / (4 % 3) - 7);
+console.log(result);
